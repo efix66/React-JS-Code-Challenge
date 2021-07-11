d39d1e6d24fd23c8c00b9c7980e507d1c7ae9fda
Efix <efix66@gmail.com>
2021-07-11 07:29:35 +0300

    Challenge finished

--- first-r-challenge/src/Input.js
+++ first-r-challenge/src/Input.js
@@ -1,6 +1,13 @@
 import React from "react";
+import colorNames from "colornames";
 
-const Input = ({ colorValue, setColorValue }) => {
+const Input = ({
+  colorValue,
+  setColorValue,
+  setHexValue,
+  isDarkText,
+  setIsDarkText,
+}) => {
   return (
     <form onSubmit={(e) => e.preventDefault()}>
       <label>Add color name</label>
@@ -10,8 +17,14 @@ const Input = ({ colorValue, setColorValue }) => {
         placeholder="Add color name"
         required
         value={colorValue}
-        onChange={(e) => setColorValue(e.target.value)}
+        onChange={(e) => {
+          setColorValue(e.target.value);
+          setHexValue(colorNames(e.target.value));
+        }}
       />
+      <button type="button" onClick={() => setIsDarkText(!isDarkText)}>
+        Dark Light mode
+      </button>
     </form>
   );
 };
